01dab0fff47a08270571c79d8f518e6fbf878aed
Kirill <traineratwot@yandex.ru>
2024-02-17 17:30:53 +0300

    1

--- src/tests/jump.test.ts
+++ src/tests/jump.test.ts
@@ -3,11 +3,11 @@ import { runFunc, runFuncJump } from "./testUtils"
 import { jump } from "../functions/jump"
 
 describe("jump", () => {
-	test("j", () => {
-		expect(runFuncJump(jump.j, [5], 10)).resolves.toBe(5)
+test("j", () => {
+	expect(runFuncJump(jump.j, [5], 10)).resolves.toBe(5)
 	})
-	test("jr", () => {
-		expect(runFuncJump(jump.jr, [1], 10)).resolves.toBe(11)
+test("jr", () => {
+	expect(runFuncJump(jump.jr, [1], 10)).resolves.toBe(11)
 	})
 	test("jal", () => {
 		expect(runFuncJump(jump.jal, [1], 10)).resolves.toBe(1)
