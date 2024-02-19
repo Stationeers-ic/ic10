568d8c70f5ec6132acc60a28f4f6913655f0be54
Aidan647 <kirillkotka@gmail.com>
2024-02-20 00:48:19 +0200

    Update conditions.ts

--- src/functions/conditions.ts
+++ src/functions/conditions.ts
@@ -4,54 +4,53 @@ import { ConditionName, StringOrNumberOrNaN } from "../ZodTypes"
 
 export const epsilon = 2 ** -23
 
-
-const eq:icCondition = (env, data) => {
+const eq: icCondition = (env, data) => {
 	const [x, y] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
 	return env.get(x) == env.get(y)
 }
-const ge:icCondition = (env, data) => {
+const ge: icCondition = (env, data) => {
 	const [x, y] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
 	return env.get(x) >= env.get(y)
 }
-const gt:icCondition = (env, data) => {
+const gt: icCondition = (env, data) => {
 	const [x, y] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
 	return env.get(x) > env.get(y)
 }
-const le:icCondition = (env, data) => {
+const le: icCondition = (env, data) => {
 	const [x, y] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
 	return env.get(x) <= env.get(y)
 }
-const lt:icCondition = (env, data) => {
+const lt: icCondition = (env, data) => {
 	const [x, y] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
 	return env.get(x) < env.get(y)
 }
-const ne:icCondition = (env, data) => !conditions.eq(env, data)
+const ne: icCondition = (env, data) => !conditions.eq(env, data)
 
-const na:icCondition = (env, data) => {
+const na: icCondition = (env, data) => {
 	const [x, y, c] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
 	return (
 		Math.abs(env.get(x) - env.get(y)) >
 		Math.max(env.get(c) * Math.max(Math.abs(env.get(x)), Math.abs(env.get(y))), epsilon)
-		)
-	}
-	const ap:icCondition = (env, data) => {
-		const [x, y, c] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
-		return (
-			Math.abs(env.get(x) - env.get(y)) <=
-			Math.max(env.get(c) * Math.max(Math.abs(env.get(x)), Math.abs(env.get(y))), epsilon)
 	)
 }
-const dse:icCondition = (env, data) => {
+const ap: icCondition = (env, data) => {
+	const [x, y, c] = z.tuple([StringOrNumberOrNaN, StringOrNumberOrNaN, StringOrNumberOrNaN]).parse(data)
+	return (
+		Math.abs(env.get(x) - env.get(y)) <=
+		Math.max(env.get(c) * Math.max(Math.abs(env.get(x)), Math.abs(env.get(y))), epsilon)
+	)
+}
+const dse: icCondition = (env, data) => {
 	const [d] = z.tuple([z.string()]).parse(data)
 	return env.hasDevice(env.getAlias(d))
 }
-const dns:icCondition = (env, data) => !conditions.dse(env, data)
+const dns: icCondition = (env, data) => !conditions.dse(env, data)
 
-const nan:icCondition = (env, data) => {
+const nan: icCondition = (env, data) => {
 	const [v] = z.tuple([StringOrNumberOrNaN]).parse(data)
 	return isNaN(env.get(v))
 }
-const nanz:icCondition = (env, data) => !conditions.nan(env, data)
+const nanz: icCondition = (env, data) => !conditions.nan(env, data)
 
 export const conditions: Record<ConditionName, icCondition> = {
 	eq,
