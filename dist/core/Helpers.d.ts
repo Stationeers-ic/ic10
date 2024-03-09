835162ebda464cefbc47c1280af5907af4169eec
Traineratwot <Traineratwot@yandex.ru>
2024-03-09 15:12:32 +0300

    build

--- dist/core/Helpers.d.ts
+++ dist/core/Helpers.d.ts
@@ -1,6 +1,13 @@
 import Environment from "../abstract/Environment";
-declare function dynamicDevicePortAsync(env: Environment, string: string): Promise<string>;
-declare function dynamicRegisterAsync(env: Environment, string: string): Promise<string>;
-declare function dynamicDevicePort(env: Environment, string: string): string;
-declare function dynamicRegister(env: Environment, string: string): string;
-export { dynamicDevicePort, dynamicRegister, dynamicDevicePortAsync, dynamicRegisterAsync };
+type PathFor = (env: Environment, string: string) => string;
+type PathForAsync = (env: Environment, string: string) => Promise<string>;
+declare const pathFor_DynamicDevicePortAsync: PathForAsync;
+declare const pathFor_DynamicRegisterAsync: PathForAsync;
+declare const pathFor_DynamicDevicePort: PathFor;
+declare const pathFor_DynamicRegister: PathFor;
+declare const pathFor_PortWithConnection: PathFor;
+declare const PortWithConnection: (port: string) => {
+    port: string;
+    connection: null | string;
+};
+export { pathFor_DynamicDevicePort, pathFor_DynamicRegister, pathFor_DynamicDevicePortAsync, pathFor_DynamicRegisterAsync, PortWithConnection, pathFor_PortWithConnection, };
