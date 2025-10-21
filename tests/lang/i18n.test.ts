359a497c64feecdbce483c78d50e2eb40d9f7cad
traineratwot <traineratwot@yandex.ru>
2025-10-21 16:50:00 +0300

    fix lang

---
+++ tests/lang/i18n.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "bun:test";
+import { Languages } from "@/Languages";
+import { i18n } from "@/Languages/lang";
+
+describe("переводы ", async () => {
+	await i18n.init({
+		lng: "en", // язык по умолчанию
+		fallbackLng: "en",
+		debug: true,
+		resources: Languages,
+	});
+
+	test("переменные", () => {
+		expect(i18n.t("error.alias_already_defined", { alias: "test" })).toBe("test is already defined");
+	});
+});
