6815e070a4cc7c17d54a0c0c872fa3c196e5f1f0
Kirill <traineratwot@yandex.ru>
2025-10-29 14:41:16 +0300

    fix

--- src/Schemas/EnvSchema.ts
+++ src/Schemas/EnvSchema.ts
@@ -177,7 +177,7 @@ export const NetworkSchema = strictObject({
 const semVerPattern =
 	/^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/;
 const ProjectSchema = object({
-	name: optional(pipe(string(), minLength(1))),
+	name: pipe(string(), minLength(1)),
 	author: optional(pipe(string(), minLength(1))),
 	description: optional(pipe(string(), minLength(1))),
 	version: optional(pipe(string(), regex(semVerPattern, "Must be SemVer eg: (1.0.0)"))),
